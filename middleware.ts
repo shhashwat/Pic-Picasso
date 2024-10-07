dacbccda3a16bc1a068c12fe26bc4a5136d31339
Shashwat <sshashwatssingh@gmail.com>
2024-10-07 17:18:09 +0530

    middleware update

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher([
 '/',
-  '/api/webhooks(.*)'
+  '/api/webhooks/clerk',
 ])
 
 export default clerkMiddleware((auth, req) => {
