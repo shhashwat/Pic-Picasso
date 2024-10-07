da68a28c73407d9c567f4ff82608eb7b1a2cc229
Shashwat <sshashwatssingh@gmail.com>
2024-10-07 12:22:50 +0530

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,3 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { clerkMiddleware } from '@clerk/nextjs/server';
 import { authMiddleware } from '@clerk/nextjs/server';
 
 // Define public routes manually
