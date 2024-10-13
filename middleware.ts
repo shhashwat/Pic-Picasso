54e51e928ce5143d8e54d4339e589fdbdcae06d7
Shashwat <sshashwatssingh@gmail.com>
2024-10-13 20:04:17 +0530

    hgfyt

--- middleware.ts
+++ middleware.ts
@@ -30,7 +30,7 @@ export default clerkMiddleware((auth, req) => {
 
 export const config = {
   matcher: [
-    '/api/webhooks/clerk', //The thing
+    '/api/webhooks/clerk',
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)', 
   ],
