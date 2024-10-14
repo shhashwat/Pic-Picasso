b0230516959939557e0facfc0ecfe2238ae26a31
Shashwat <sshashwatssingh@gmail.com>
2024-10-14 16:24:45 +0530

    deasfs

--- middleware.ts
+++ middleware.ts
@@ -29,7 +29,7 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: [
+  matcher: [ //dscf
     '/api/webhooks/clerk',
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)', 
