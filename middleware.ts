f2f1895317ad65b5b9eb33da287f74f8ba7f6f26
Shashwat <sshashwatssingh@gmail.com>
2024-10-15 17:53:48 +0530

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,37 +1,18 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-// const isProtectedRoute = createRouteMatcher([
-// '/',
-// '/api/clerk',
-// ])
+const isPublicRoute = createRouteMatcher(['/api/webhooks/clerk'])
 
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect()
-// })
-
-// export const config = {
-//   matcher: [
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     '/(api|trpc)(.*)',
-//   ],
-// }
-
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-const isProtectedRoute = createRouteMatcher([
-  '/',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
   }
-});
+})
 
 export const config = {
-  matcher: [ //dscf
-    '/api/webhooks/clerk',
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)', 
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
-};
+}
